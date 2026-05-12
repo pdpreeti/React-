6a9213391ca651a6f742a38c5e2a6254c77eac4f
Preeti Desai <pd.preeti26042005@gmail.com>
2026-05-12 11:43:02 +0530

    first commit

---
+++ Folder 1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child'>Hello World</h1>
+      <h2 id='child 1'>Hello World</h2>
+    </div>
+    <div id="uncle">
+      <h1 id="nephew">Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
